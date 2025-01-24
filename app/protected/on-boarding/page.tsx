5d80ac22dcdf26134da63647a58c695e3a22b4a6
Mahdi1bogh <mahdi.boughanmi@hotmail.com>
2025-01-24 06:42:32 +0100

    ui centering

--- app/protected/on-boarding/page.tsx
+++ app/protected/on-boarding/page.tsx
@@ -5,18 +5,20 @@ import Typography from "@/components/ui/typography";
 export default async function Page() {
   const user = await getUserData();
   return (
-    <div>
-      <Typography
-        text={`👋 Welcome ${user?.name || user?.email}`}
-        variant="h3"
-      />
-      <Typography
-        text="We are gonna build your portfolio now , it will take few seconds"
-        variant="p"
-        className="my-3"
-      />
-      <Typography text="Let's get Started" variant="h3" className="my-3" />
-      <OnBoardingForm />
+    <div className="w-full grid place-content-center">
+      <div className="flex flex-col  max-w-5xl p-5">
+        <Typography
+          text={`👋 Welcome ${user?.name || user?.email}`}
+          variant="h3"
+        />
+        <Typography
+          text="We are gonna build your portfolio now , it will take few seconds"
+          variant="p"
+          className="my-3"
+        />
+        <Typography text="Let's get Started" variant="h3" className="my-3" />
+        <OnBoardingForm />
+      </div>
     </div>
   );
 }
